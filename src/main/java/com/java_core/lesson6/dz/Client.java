2b2af2dd6ed0c012a234f10fe2c5ec59230d59a2
Ruslan <R76987R@yandex.ru>
2018-07-25 20:45:02 +0300

    lesson6 dz done

---
+++ src/main/java/com/java_core/lesson6/dz/Client.java
@@ -0,0 +1,21 @@
+package com.java_core.lesson6.dz;
+
+import java.io.*;
+import java.net.Socket;
+
+public final class Client {
+
+    public static void main(String[] args) {
+        new Client().runApplication();
+    }
+
+    private void runApplication() {
+        try {
+            final Socket socket = new Socket(Server.SOCKET_ADDR, Server.SOCKET_PORT);
+            final MyThreads myThreads = new MyThreads(socket);
+            myThreads.runApplication();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
