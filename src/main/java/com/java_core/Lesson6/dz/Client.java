b08efbf76d29ea691ebe0d9f8374a4034832fd81
Ruslan <R76987R@yandex.ru>
2018-07-25 20:37:10 +0300

    lesson6 class Client dz change

--- src/main/java/com/java_core/Lesson6/dz/Client.java
+++ src/main/java/com/java_core/Lesson6/dz/Client.java
@@ -11,8 +11,8 @@ public final class Client {
 
     private void runApplication() {
         try {
-            final Socket socket = new Socket(Server.SOCKET_ADDR, Server.SOCKET_PORT);
-            final MyThreads myThreads = new MyThreads(socket);
+            final Socket socket = new Socket(com.java_core.lesson6.dz.Server.SOCKET_ADDR, com.java_core.lesson6.dz.Server.SOCKET_PORT);
+            final com.java_core.lesson6.dz.MyThreads myThreads = new com.java_core.lesson6.dz.MyThreads(socket);
             myThreads.runApplication();
         } catch (IOException e) {
             e.printStackTrace();
