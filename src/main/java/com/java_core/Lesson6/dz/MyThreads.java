25c324a50d6dc1677dda2d5533356eefc14c8ac5
Ruslan <R76987R@yandex.ru>
2018-07-25 20:22:56 +0300

    lesson 6 dz change

---
+++ src/main/java/com/java_core/Lesson6/dz/MyThreads.java
@@ -0,0 +1,70 @@
+package com.java_core.lesson6.dz;
+
+import java.io.*;
+import java.net.Socket;
+
+public final class MyThreads {
+
+    private Socket socket;
+
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
+    private BufferedReader bufferedReader;
+
+    public MyThreads(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void runApplication() {
+        try {
+            inputStream = new DataInputStream(socket.getInputStream());
+            outputStream = new DataOutputStream(socket.getOutputStream());
+            bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            new Thread(new ThreadSockedIn()).start();
+            new Thread(new ThreadSockedOut()).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private final class ThreadSockedIn implements Runnable {
+
+        public void run() {
+            while (true) {
+
+                if (!socket.isConnected()) {
+                    break;
+                }
+
+                try {
+                    final String messageIn = inputStream.readUTF();
+                    System.out.println("message: " + messageIn);
+                } catch (IOException e) {
+                    System.out.println("Кличент отключился");
+                    break;
+                }
+            }
+        }
+    }
+
+    private final class ThreadSockedOut implements Runnable {
+
+        public void run() {
+            while (true) {
+
+                if (!socket.isConnected()) {
+                    break;
+                }
+
+                try {
+                    final String messageOut = bufferedReader.readLine();
+                    outputStream.writeUTF(messageOut);
+                    outputStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
+        }
+    }
+}
